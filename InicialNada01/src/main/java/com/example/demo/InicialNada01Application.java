82f2b5c2372289cf09aec87f71c05261af7b521e
pabloieshna <joesingleton417@gmail.com>
2024-01-10 13:12:39 +0100

    inicial

---
+++ InicialNada01/src/main/java/com/example/demo/InicialNada01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicialNada01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicialNada01Application.class, args);
+	}
+
+}
