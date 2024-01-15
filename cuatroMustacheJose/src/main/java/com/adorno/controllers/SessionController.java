4f0b2c0da550c66d9a8210ca094a7377d2ae6c2a
pabloieshna <joesingleton417@gmail.com>
2024-01-15 09:46:37 +0100

    cuatro mustache

--- cuatroMustacheJose/src/main/java/com/adorno/controllers/SessionController.java
+++ cuatroMustacheJose/src/main/java/com/adorno/controllers/SessionController.java
@@ -1,5 +1,32 @@
 package com.adorno.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
 public class SessionController {
 
+	@RequestMapping("dimeAlgo")
+	//vamos a meter en el controller una informacion que viene de la peticion
+	public String giveMe(Model model,@RequestParam String parametro) {
+		model.addAttribute("parametro", parametro);
+		return "saySomething";
+	}
+	
+	@RequestMapping(value = "/procesarFormulario", method = RequestMethod.POST)
+	public String processForm(@RequestParam String info, HttpSession session) {
+		session.setAttribute("infoUser", info);
+		return "resultado_formulario";
+	}
+	
+	@RequestMapping("/mostrarDatos")
+	public String mostrarDatos(Model model, HttpSession sesion) {
+		model.addAttribute("infoUsuario", sesion.getAttribute("infoUser"));
+		return "datos";
+	}
 }
