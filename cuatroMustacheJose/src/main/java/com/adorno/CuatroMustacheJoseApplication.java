8b15a80790c5928f4b12eeb5512dd6a7dbcdcc7e
pabloieshna <joesingleton417@gmail.com>
2024-01-15 09:13:05 +0100

    cuatromustache

---
+++ cuatroMustacheJose/src/main/java/com/adorno/CuatroMustacheJoseApplication.java
@@ -0,0 +1,13 @@
+package com.adorno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuatroMustacheJoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuatroMustacheJoseApplication.class, args);
+	}
+
+}
---
+++ cuatroMustacheJose/src/main/java/com/adorno/controllers/SessionController.java
@@ -0,0 +1,5 @@
+package com.adorno.controllers;
+
+public class SessionController {
+
+}
