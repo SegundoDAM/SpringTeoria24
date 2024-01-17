72d0b6bc5fbc6ca2e5ca7c552ba9f8f3f8b18ba0
pabloieshna <joesingleton417@gmail.com>
2024-01-17 14:00:27 +0100

    api rest 2

--- ApiRestSolitario03/src/main/java/com/adorno/controllers/PersonaController.java
+++ ApiRestSolitario03/src/main/java/com/adorno/controllers/PersonaController.java
@@ -1,6 +1,9 @@
 package com.adorno.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +21,13 @@ public class PersonaController {
 		this.personaService = personaService;
 	}
 
-	@GetMapping("busqueda")
-	public Persona encontrarPersona() {
-		return personaService.getByDNI("1").orElse(new Persona());
+	@GetMapping("busqueda/{DNI}")
+	public Persona encontrarPersona(@PathVariable String DNI) {
+		return personaService.getByDNI(DNI).orElse(new Persona());
+	}
+	
+	@PostMapping("insercion")
+	public boolean insertar(@RequestBody Persona persona) {
+		return personaService.add(persona);
 	}
 }
