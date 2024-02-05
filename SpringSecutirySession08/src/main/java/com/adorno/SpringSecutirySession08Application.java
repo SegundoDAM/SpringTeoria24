fc8ed866ce8f0012bfb78dc8d6d89b5676aa5d4b
pabloieshna <joesingleton417@gmail.com>
2024-02-05 09:03:14 +0100

    httpbasic ends

---
+++ SpringSecutirySession08/src/main/java/com/adorno/SpringSecutirySession08Application.java
@@ -0,0 +1,13 @@
+package com.adorno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecutirySession08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecutirySession08Application.class, args);
+	}
+
+}
