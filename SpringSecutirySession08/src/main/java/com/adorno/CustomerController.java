df05c4bf2ffdd6bf8367c4f612ee5935da944713
pabloieshna <joesingleton417@gmail.com>
2024-02-05 09:36:28 +0100

    registro de session

--- SpringSecutirySession08/src/main/java/com/adorno/CustomerController.java
+++ SpringSecutirySession08/src/main/java/com/adorno/CustomerController.java
@@ -1,5 +1,12 @@
 package com.adorno;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +15,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("v1")
 public class CustomerController {
 
+	private final SessionRegistry sessionRegistry;
+
+	public CustomerController(SessionRegistry sessionRegistry) {
+		super();
+		this.sessionRegistry = sessionRegistry;
+	}
+
 //	Aqui aplicamos la seguridad
 	@GetMapping("index")
 	public String index() {
 		return "hello secured";
 	}
-	
+
 //	aqui no seguridad
 	@GetMapping("index2")
 	public String index2() {
 		return "hello unsecured";
 	}
-	
+
+	// al parecer este endpoint se hace para poder ver la infgormacion de la session
+	// que ha sido registrada
+	@GetMapping("session")
+	public ResponseEntity<?> getRegistrySession() {
+		List<Object> allPrincipals = sessionRegistry.getAllPrincipals();
+		User user = (User) allPrincipals.stream().filter(sess -> {
+			return sess instanceof User;
+		}).findFirst().get();
+		List<SessionInformation> allSessions = sessionRegistry.getAllSessions(user, true);
+		HashMap<String, Object> response = new HashMap<>();
+		response.put("sessionId", allSessions.get(allSessions.size() - 1).getSessionId());
+		response.put("user", user);
+		return ResponseEntity.ok(response);
+	}
 }
--- SpringSecutirySession08/src/main/java/com/adorno/SecurityConfig.java
+++ SpringSecutirySession08/src/main/java/com/adorno/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -11,25 +14,67 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 @EnableWebSecurity
 public class SecurityConfig {
 
-
-	public AuthenticationSuccessHandler getSuccess(){
-		return ((request,response,authentication)->{
+	public AuthenticationSuccessHandler getSuccess() {
+		return ((request, response, authentication) -> {
 			response.sendRedirect("/v1/index");
 		});
 	}
-	
+
+	@Bean
+	SessionRegistry createSessionRegistry() {
+		return new SessionRegistryImpl();
+	}
+
 	@Bean
 	SecurityFilterChain getFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity
-				.authorizeHttpRequests((auth) -> {
-							auth.requestMatchers("v1/index2").permitAll();
-							auth.anyRequest().authenticated();
-						})
-				//todos tienen permiso de acceso al login
+		return httpSecurity.authorizeHttpRequests((auth) -> {
+			auth.requestMatchers("v1/index2").permitAll();
+			auth.anyRequest().authenticated();
+		})
+				// todos tienen permiso de acceso al login
 				.formLogin(login -> {
 					login.permitAll();
 					login.successHandler(getSuccess());
-					})
+				}).sessionManagement(sess -> {
+					// hay varios tipos
+
+					// always siempre que no hay una session existente la creara, si la hay la
+					// reutiliza
+					sess.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+					// crea una session si es necesario si no hay session la crea
+					// hacen casi lo mismo. este es mas restrictivo(?) evalua si hace falta crear la
+					// sesion
+					// sess.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+					// no creamos sesiones pero reutiliza las que haya
+					// sess.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+					// no crea ni reutiliza. Todas las peticiones son atomicas
+					// sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+					// las sesiones permiten almacenar informacion sobre el usuario, en esa session
+					/*
+					 * }) /* Aquii podemos decir por defecto la uri a la que vamos si no se crea
+					 * bien la session
+					 */
+					sess.invalidSessionUrl("/login");
+					/*
+					 * Cuantas sesiones a la vez? una o varias dependera del servidor
+					 */
+//								sess.maximumSessions(1);
+//								Esto esta asi porque el metodo no aparece en sessionMangement
+					// Esta informacion esta accesible. Creamos otro endpoiint
+					//
+					sess.maximumSessions(1).sessionRegistry(createSessionRegistry());
+					// esto controla cuando ha expirado la session. esta quitada
+//					sess.expiredURL();
+					// Es una vulnerabilidad con las sesiones
+					sess.sessionFixation()
+							// ante un ataque genera otro id. y copiar los datos de la sesion a la nueva
+							// sesion
+							//.migrateSession();
+							// igual que la anterior sin migrar los datos
+							// .newSession();
+							// no hacer nada (????!!!!) deshabilitamos este escudo
+							.none();
+							})
 				.build();
 	}
 }
